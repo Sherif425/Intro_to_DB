2d4e691fbaa8bd440b05a96ce05f25412a60fc95
Sherif Mohamed <sherif425@gmail.com>
2025-08-12 10:15:51 +0300

    creating the database alx_book_store in your MySQL server

--- MySQLServer.py
+++ MySQLServer.py
@@ -8,7 +8,7 @@ try:
         # database = "alx_book_store"    
     ) as mysql_db:
         myCursor = mysql_db.cursor()
-        myCursor.execute("Create Database IF NOT EXISTS alx_book_store ")       
+        myCursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")       
         print("Database 'alx_book_store' created successfully!")
 except mysql.connector.Error as err:
     print(f"Error: {err}")    
